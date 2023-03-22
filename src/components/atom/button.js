2debac97d21d243407e6786d04f5b4cc8594dd14
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-03-22 19:03:54 +0530

    initial commit

---
+++ src/components/atom/button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './button.scss'
+
+function Button() {
+  return (
+    <div>
+       <a href="https://sc.com/bd/" class="btn-dl">Get Tickets Now</a>
+    </div>
+  )
+}
+
+export default Button

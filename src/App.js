2282aef24cc93a97fc7d888631ccbfb84b4dd6b8
Fajarudheen PA <fajarudheen98@gmail.com>
2023-04-29 22:41:48 +0530

    changes have been updated in project - restarted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import NavBar from './components/NavBar/NavBar';
-// import Banner from './components/Banner/Banner';
+import Banner from './components/Banner/Banner';
 import './App.css';
 
 function App() {
   return (
     <div>
       <NavBar/>
-      {/* <Banner/> */}
+      <Banner/>
     </div>
   );
 }

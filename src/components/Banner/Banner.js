d929210afaaa2eb3f1f04cf92de2100b95900e8f
Fajarudheen PA <fajarudheen98@gmail.com>
2023-05-03 23:48:17 +0530

    updated banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Banner.scss'
+import { Link } from 'react-router-dom'
 
 function Banner() {
   return (
@@ -12,9 +13,9 @@ function Banner() {
                     <p className='banner__desc'>Lets make it through <span style={{fontWeight:'600', color:'white'}}>Ledger Web.</span> We offer the best travel deals and experiences for your next adventure. Our team of experts is dedicated to helping you plan the perfect trip, and our user-friendly platform makes it easy to book flights, visa and passport services all in one place.</p>
                 </div>
                 <div className='banner__right'>
-                    <div className='banner__explore-bg'>
+                    <Link to='/' className='banner__explore-bg'>
                         <p className='banner__explore-p'>EXPLORE</p>
-                    </div>
+                    </Link>
                 </div>
             </div>
         </div>

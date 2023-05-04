34fe72fef9333f67dea6ded756de964fba13a2a0
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-04 17:33:27 +0530

    updated index.js and app.js with routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import NavBar from './components/NavBar/NavBar';
-import Banner from './components/Banner/Banner';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <NavBar/>
-      <Banner/>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }

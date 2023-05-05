5fa5c6dbfa528582f861644d01976c264f135e2d
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-05 18:18:23 +0530

    added overview to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar/NavBar'
 import Banner from '../components/Banner/Banner'
+import Overview from '../components/Overview/Overview'
 
 function Home() {
   return (
     <div>
         <NavBar/>
         <Banner/>
+        <Overview/>
     </div>
   )
 }

fd464fad33649efa20dc6adaaa2a69462778db18
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-08 18:36:24 +0530

    updated Overview module

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './Overview.scss'
 
 function Overview() {
   return (
     <div className='overview'>
         <div className="content-wrapper">
             <div className="overview__wrap">
-                
+                <div className="overview__left">
+                  <h2>Get Experience Which are fun</h2>
+                  <p>With <strong>ledger Webb</strong> you can get the best experience on holiday travel, and we always update the latest and best and fair prices in the market. </p>
+                </div>
+                <div className="overview__right">
+
+                </div>
             </div>
         </div>
     </div>

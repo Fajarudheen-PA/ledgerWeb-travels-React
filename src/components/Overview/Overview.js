cdf0ccf58e9492c684a09dd2864e787baa9d2b3a
Fajarudheen PA <fajarudheen98@gmail.com>
2023-05-09 07:42:08 +0530

    updated Overview

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -7,8 +7,8 @@ function Overview() {
         <div className="content-wrapper">
             <div className="overview__wrap">
                 <div className="overview__left">
-                  <h2>Get Experience Which are fun</h2>
-                  <p>With <strong>ledger Webb</strong> you can get the best experience on holiday travel, and we always update the latest and best and fair prices in the market. </p>
+                  <h2 className='overview__main-header'>Get Experience Which are fun</h2>
+                  <p className='overview__desc'>With <strong>ledger Webb</strong> you can get the best experience on holiday travel, and we always update the latest and best and fair prices in the market. </p>
                 </div>
                 <div className="overview__right">
 

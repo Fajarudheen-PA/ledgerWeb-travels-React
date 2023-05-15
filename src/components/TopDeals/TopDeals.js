938f1423d26b8c6139605fe00297f32c79e66cec
Fajarudheen PA <fajarudheen98@gmail.com>
2023-05-15 22:37:18 +0530

    changes in TopDeals module

--- src/components/TopDeals/TopDeals.js
+++ src/components/TopDeals/TopDeals.js
@@ -9,7 +9,18 @@ function TopDeals() {
         <div className="topdeals__card-group">
           {/* card */}
           <div className="topdeals__card">
-            <img src="" alt="Offer-1" />
+            <img src="../../../images/ledger-deals-dubai.jpg" alt="Offer-1" className='topdeals__card__image'/>
+            <div>
+              <h4 className='topdeals__card__head'>Kochi - Dubai</h4>
+              <p className='topdeals__card__desc'>The red and orange sand of the desert are very beautiful, let's take a trip here.</p>
+            </div>
+            <div className='topdeals__card__price'>
+              <div className="topdeals__card__price__left">
+                <p>Price</p>
+                <p>₹20,000/-</p>
+              </div>
+              <div className="topdeals__card__price__right"></div>
+            </div>
           </div>
         </div>
       </div>

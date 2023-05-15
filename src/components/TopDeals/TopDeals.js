3ca14e8b52779ec86fac45ed3123b0aebd4590c0
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-15 15:17:07 +0530

    changes to TopDeals module

--- src/components/TopDeals/TopDeals.js
+++ src/components/TopDeals/TopDeals.js
@@ -6,6 +6,12 @@ function TopDeals() {
     <div className='topdeals'>
       <div className="content-wrapper">
         <h3 className='topdeals__main-head'>Today's Top Deals</h3>
+        <div className="topdeals__card-group">
+          {/* card */}
+          <div className="topdeals__card">
+            <img src="" alt="Offer-1" />
+          </div>
+        </div>
       </div>
     </div>
   )

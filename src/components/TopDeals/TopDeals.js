68467c6a5c1e6921780dd6ee8a3e91409a4e2b24
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-16 17:32:09 +0530

    changes in TopDeals module

--- src/components/TopDeals/TopDeals.js
+++ src/components/TopDeals/TopDeals.js
@@ -8,6 +8,7 @@ function TopDeals() {
       <div className="content-wrapper">
         <h3 className='topdeals__main-head'>Today's Top Deals</h3>
         <div className="topdeals__card-group">
+          
           {/* card */}
           <div className="topdeals__card">
             <img src="../../../images/ledger-deals-dubai.jpg" alt="Offer-1" className='topdeals__card__image'/>
@@ -21,12 +22,47 @@ function TopDeals() {
                 <p className='topdeals__card__price__head topdeals__card__price__head--rate'>₹20,000/-</p>
               </div>
               <div className="topdeals__card__price__right">
-                <div>
-                  <Link to='/' className='topdeals__card__btn'>Ticket Booking</Link>
-                </div>
+                <Link to='/' className='topdeals__card__btn'>Ticket Booking</Link>
               </div>
             </div>
           </div>
+
+          {/* card */}
+          <div className="topdeals__card">
+            <img src="../../../images/ledger-deals-dubai.jpg" alt="Offer-1" className='topdeals__card__image'/>
+            <div>
+              <h4 className='topdeals__card__head'>Kochi - Dubai</h4>
+              <p className='topdeals__card__desc'>The red and orange sand of the desert are very beautiful, let's take a trip here.</p>
+            </div>
+            <div className='topdeals__card__price'>
+              <div className="topdeals__card__price__left">
+                <p className='topdeals__card__price__head'>Price</p>
+                <p className='topdeals__card__price__head topdeals__card__price__head--rate'>₹20,000/-</p>
+              </div>
+              <div className="topdeals__card__price__right">
+                <Link to='/' className='topdeals__card__btn'>Ticket Booking</Link>
+              </div>
+            </div>
+          </div>
+
+          {/* card */}
+          <div className="topdeals__card">
+            <img src="../../../images/ledger-deals-dubai.jpg" alt="Offer-1" className='topdeals__card__image'/>
+            <div>
+              <h4 className='topdeals__card__head'>Kochi - Dubai</h4>
+              <p className='topdeals__card__desc'>The red and orange sand of the desert are very beautiful, let's take a trip here.</p>
+            </div>
+            <div className='topdeals__card__price'>
+              <div className="topdeals__card__price__left">
+                <p className='topdeals__card__price__head'>Price</p>
+                <p className='topdeals__card__price__head topdeals__card__price__head--rate'>₹20,000/-</p>
+              </div>
+              <div className="topdeals__card__price__right">
+                <Link to='/' className='topdeals__card__btn'>Ticket Booking</Link>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>

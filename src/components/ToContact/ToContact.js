2f65f9a77a97474ee2b68f4ce04707a174d1d59c
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-18 18:24:54 +0530

    minor update on ToContact module

--- src/components/ToContact/ToContact.js
+++ src/components/ToContact/ToContact.js
@@ -3,8 +3,15 @@ import './ToContact.scss'
 
 function ToContact() {
   return (
-    <div>
-
+    <div className='tocontact'>
+        <div className="content-wrapper">
+            <div className="tocontact__wrap">
+                <div className="tocontact__left">
+                    <h3 className="tocontact__main-head">We are ready to provide the best trip for you</h3>
+                </div>
+                <div className="tocontact__right"></div>
+            </div>
+        </div>
     </div>
   )
 }

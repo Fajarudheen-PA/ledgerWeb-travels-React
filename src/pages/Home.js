608fc89cc01c420b9d73584ed8f27029942ca4e6
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-18 15:38:46 +0530

    added ToContact to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import NavBar from '../components/NavBar/NavBar'
 import Banner from '../components/Banner/Banner'
 import Overview from '../components/Overview/Overview'
 import TopDeals from '../components/TopDeals/TopDeals'
+import ToContact from '../components/ToContact/ToContact'
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
         <Banner/>
         <Overview/>
         <TopDeals/>
+        <ToContact/>
     </div>
   )
 }

5aa74b6ac85e43e12e59c719563e2a2c4e9740a9
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-19 18:30:46 +0530

    updates to ToContact module, need to make it responsive

--- src/components/ToContact/ToContact.js
+++ src/components/ToContact/ToContact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './ToContact.scss'
 
 function ToContact() {
@@ -8,8 +9,12 @@ function ToContact() {
             <div className="tocontact__wrap">
                 <div className="tocontact__left">
                     <h3 className="tocontact__main-head">We are ready to provide the best trip for you</h3>
+                    <p className='tocontact__desc'>We have a variety of the world's best destinations that you can choose as your trip destination</p>
+                    <Link to='/' className='tocontact__btn'>Contact Now</Link>
                 </div>
-                <div className="tocontact__right"></div>
+                <div className="tocontact__right">
+                  <img src="../../../images/ledger-tocontact.jpg" alt="Contact" className='tocontact__image'/>
+                </div>
             </div>
         </div>
     </div>

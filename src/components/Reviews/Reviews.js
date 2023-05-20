7530d7e938b78504f7178adc537db2c8bb9b401c
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-20 12:23:11 +0530

    created Reviews module

---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Reviews() {
+  return (
+    <div className='reviews'>
+        <div className="content-wrapper">
+            <div className="reviews__wrap">
+                <div className="reviews__left"></div>
+                <div className="reviews__right"></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Reviews

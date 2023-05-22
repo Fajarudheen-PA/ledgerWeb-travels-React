af2f82395556cf052589c018b8d4ed7400291ea8
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-22 18:28:49 +0530

    changes on Review module

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './Reviews.scss';
 
 function Reviews() {
+    var settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }; 
   return (
     <div className='reviews'>
         <div className="content-wrapper">
             <div className="reviews__wrap">
-                <div className="reviews__left"></div>
-                <div className="reviews__right"></div>
+                <div className="reviews__left">
+                    <Slider {...settings}>
+                        <div className='reviews__card'>
+                            <img src="" alt="Customer 1" />
+                        </div>
+                    </Slider>
+                </div>
+                <div className="reviews__right">
+                    <h3 className='reviews__main-head'>Testimonials<br/><span className='reviews__main-head reviews__main-head--sub-head'>Ledger Webb Customers</span></h3>
+                    <p className='reviews__desc'>What our customers have to say about our service, check now!</p>
+                </div>
             </div>
         </div>
     </div>

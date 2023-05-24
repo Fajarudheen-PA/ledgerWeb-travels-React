ae5bb2ba06f7fe013b9fddc461ed798f67d73871
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-24 18:25:57 +0530

    Created Footer module

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.scss';
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className="content-wrapper">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Footer

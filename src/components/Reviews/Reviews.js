528f1e15728c2ef7fa1669dccda867540e900b27
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-24 17:34:30 +0530

    added dots to carousel in Review module

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -9,6 +9,7 @@ import './Reviews.scss';
 function Reviews() {
     var settings = {
         infinite: true,
+        dots: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,

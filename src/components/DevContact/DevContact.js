9f78092ede8a9d78e1be343b10aa4f601e6341c5
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-26 11:12:10 +0530

    Created DevContact module

---
+++ src/components/DevContact/DevContact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './DevContact.scss'
+import { Link } from 'react-router-dom'
+
+function DevContact() {
+  return (
+    <div className='devcontact'>
+        <div className="content-wrapper">
+            <p><Link>Developer Contact</Link></p>
+        </div>
+    </div>
+  )
+}
+
+export default DevContact

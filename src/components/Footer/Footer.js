665dd9f5d6bfaeb339f4e213a436b33b290cf16b
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-05-26 10:55:01 +0530

    Footer module updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faWhatsapp, faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import './Footer.scss';
 
 function Footer() {
-    const url = "https://wa.me/qr/7O32GILDT5YYA1";
+    const waUrl = "https://wa.me/qr/7O32GILDT5YYA1";
+    const instUrl = "https://instagram.com/ledgerwebb_travel_partner?igshid=MzNlNGNkZWQ4Mg==";
   return (
     <div className='footer'>
         <div className="content-wrapper">
@@ -20,16 +21,22 @@ function Footer() {
                     <p className='footer__desc'>Unlock the wonders of the world with Ledger Webb and create cherished memories that will last a lifetime.</p>
                 </div>
                 <div className="footer__right">
-                    <div className='footer__contact-us'>
-                        <h4 className='footer__contact-us__head'>Contact US</h4>
-                        <p><Link to='tel:+91XXXXXXXXXX' className='footer__contact-us__link'><FontAwesomeIcon icon={faPhone} className='footer__contact-us__link footer__contact-us__link--svg' /> +91 XXXXXXXXXX</Link></p>
-                        <p><Link to='mailto:ledgerwebbcok@gmail.com' className='footer__contact-us__link'><FontAwesomeIcon icon={faEnvelope} className='footer__contact-us__link footer__contact-us__link--svg' /> ledgerwebbcok@gmail.com</Link></p>
-                        <p><Link to={url} className='footer__contact-us__link'><FontAwesomeIcon icon={faWhatsapp} className='footer__contact-us__link footer__contact-us__link--svg' /> WhatsApp</Link></p>
+                    <div className='footer__link-grp'>
+                        <h4 className='footer__link-grp__head'>Contact US</h4>
+                        <p><Link to='tel:+91XXXXXXXXXX' className='footer__link-grp__link'><FontAwesomeIcon icon={faPhone} className='footer__link-grp__link footer__link-grp__link--svg' /> +91 XXXXXXXXXX</Link></p>
+                        <p><Link to='mailto:ledgerwebbcok@gmail.com' className='footer__link-grp__link'><FontAwesomeIcon icon={faEnvelope} className='footer__link-grp__link footer__link-grp__link--svg' /> ledgerwebbcok@gmail.com</Link></p>
+                        <p><Link to={waUrl} className='footer__link-grp__link'><FontAwesomeIcon icon={faWhatsapp} className='footer__link-grp__link footer__link-grp__link--svg' /> WhatsApp</Link></p>
                     </div>
-                    <div className="footer__follow-us">
-
+                    <div className="footer__link-grp">
+                        <h4 className='footer__link-grp__head'>Follow US</h4>
+                        <p><Link to={instUrl} className='footer__link-grp__link'><FontAwesomeIcon icon={faFacebookF} className='footer__link-grp__link footer__link-grp__link--svg' /> Facebook</Link></p>
+                        <p><Link to={instUrl} className='footer__link-grp__link'><FontAwesomeIcon icon={faTwitter} className='footer__link-grp__link footer__link-grp__link--svg' /> Twitter</Link></p>
+                        <p><Link to={instUrl} className='footer__link-grp__link'><FontAwesomeIcon icon={faInstagram} className='footer__link-grp__link footer__link-grp__link--svg' /> Instagram</Link></p>
                     </div>
                 </div>
+            </div>
+            <div className='footer__copyright'>
+                <p className='footer__copyright footer__copyright--font'>Copyright &copy; ledger Webb 2023</p>
             </div>
         </div>
     </div>

9f082fa25b51e275f66b714ec3a5a11ba5ed041d
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-06-01 12:35:48 +0530

    created Services.js in pages

---
+++ src/pages/Services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/NavBar/NavBar'
+import Footer from '../components/Footer/Footer'
+
+function Services() {
+  return (
+    <div>
+        <NavBar/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Services

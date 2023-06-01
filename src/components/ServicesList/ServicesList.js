e7a4e22dd3518c16af12c7eb24c15a4968b4d756
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-06-01 17:02:53 +0530

    Renamed Services to ServicesList in components

---
+++ src/components/ServicesList/ServicesList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ServicesList.scss'
+
+function ServicesList() {
+  return (
+    <div className='serviceslist'>
+        <div className="content-wrapper"></div>
+    </div>
+  )
+}
+
+export default ServicesList

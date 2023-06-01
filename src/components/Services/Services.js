dbe48ad87385bf3f04fa62d26ba2e5581e2d8a5b
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-06-01 16:55:23 +0530

    Created Services.js file in components

---
+++ src/components/Services/Services.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Services.scss'
+
+function Services() {
+  return (
+    <div className='services'>
+        <div className="content-wrapper"></div>
+    </div>
+  )
+}
+
+export default Services

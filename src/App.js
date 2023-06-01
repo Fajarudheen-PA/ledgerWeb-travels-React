79e57b27094865e42b820b8048c9eb27fa4c244e
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-06-01 12:41:04 +0530

    Created root for Services in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Services from './pages/Services';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path='/' element={<Home />}/>
+          <Route path='/services' element={<Services />}/>
         </Routes>
       </Router>
     </div>

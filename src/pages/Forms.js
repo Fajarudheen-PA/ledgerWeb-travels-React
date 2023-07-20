994128e3a7bac5a086dfe8616221202caa6dd3fe
Fajarudheen PA <fajarudheen98@gmail.com>
2023-07-20 10:12:38 +0530

    Added FormDoc to Form.js file

--- src/pages/Forms.js
+++ src/pages/Forms.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from '../components/NavBar/NavBar'
 import Footer from '../components/Footer/Footer'
+import FormsDoc from '../components/Forms/FormsDoc'
 
 function Forms() {
   return (
     <div>
         <NavBar/>
+        <FormsDoc/>
         <Footer/>
     </div>
   )

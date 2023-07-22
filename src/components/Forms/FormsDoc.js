3ca6fe380ab60bd6074fed97e48ab09d49ffbd0b
Fajarudheen PA <fajarudheen98@gmail.com>
2023-07-22 12:34:00 +0530

    updated FromDoc file with radio button and added popup for the number of passengers

--- src/components/Forms/FormsDoc.js
+++ src/components/Forms/FormsDoc.js
@@ -4,17 +4,22 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './FormsDoc.scss'
-// import PopUp from '../PopUp/PopUp';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faHeart } from "@fortawesome/free-regular-svg-icons";
-import { faClose, faGear, faHeadset } from "@fortawesome/free-solid-svg-icons";
+// // import PopUp from '../PopUp/PopUp';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faUser, faHeart } from "@fortawesome/free-regular-svg-icons";
+// import { faClose, faGear, faHeadset } from "@fortawesome/free-solid-svg-icons";
 
 function FormsDoc() {
 
-    const [pop, setPop] = useState(false)
+    const [pop, setPop] = useState(false);
+    const [tripType, setTripType] = useState("Round Trip");
 
     const closePopUp = () => {
         setPop(false);
+    };
+
+    const handleTripTypeChange = (e) => {
+        setTripType(e.target.value);
     };
 
     return (
@@ -24,18 +29,47 @@ function FormsDoc() {
                     <h2 className="formsdoc__main-head">Let us know</h2>
                     <form>
                         <div className="formsdoc__label-grp">
+
+                            {/* One way or Round trip radio */}
+                            {/* <label for='ttype'>Type</label> */}
+                            <div className="formsdoc__radio-grp">
+                                <label>
+                                    <input
+                                        type="radio"
+                                        value="oneWay"
+                                        checked={tripType === 'oneWay'}
+                                        onChange={handleTripTypeChange}
+                                        className='formsdoc__radio-grp__input'
+                                    />
+                                    One Way
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        value="roundTrip"
+                                        checked={tripType === 'roundTrip'}
+                                        onChange={handleTripTypeChange}
+                                        className='formsdoc__radio-grp__input'
+                                    />
+                                    Round Trip
+                                </label>
+                            </div>
+
+                            {/* From */}
                             <label for='from' className="form-label">From:</label>
                             <select className='form-select' id='from'>
                                 <option value='Cochin' selected>Cochin</option>
                                 <option value='Kozhikode'>Kozhikode</option>
                             </select>
 
+                            {/* Going To */}
                             <label for='to' className="form-label">To:</label>
                             <select className='form-select' id='to'>
                                 <option value='Cochin' selected>Cochin</option>
                                 <option value='Kozhikode'>Kozhikode</option>
                             </select>
 
+                            {/* Departure date */}
                             <label for="departdate" class="form-label">Departure Date:</label>
                             <div className="react-datepicker-wrapper">
                                 <DatePicker
@@ -48,23 +82,22 @@ function FormsDoc() {
                                 />
                             </div>
 
-                            <label for="returndate" class="form-label">Return Date:</label>
-                            <div className="react-datepicker-wrapper">
+                            {/* Return date */}
+                            {tripType === "roundTrip" ? <><label for="returndate" class="form-label">Return Date:</label><div className="react-datepicker-wrapper">
                                 <DatePicker
                                     id="returndate"
                                     name="returndate"
                                     selected={null} // Set the selected date value here
                                     onChange={() => { }} // Add onChange handler if needed
                                     placeholderText="Select a date"
-                                    className='form-select'
-                                />
-                            </div>
+                                    className='form-select' />
+                            </div></> : ""}
 
-                            {/* try */}
-                            {/* <label for="returndate" class="form-label">Number of passengers:</label> */}
+                            {/* Number of Passengers */}
                             <label for="passenger-count" className='form-label'>Number of Passengers:</label>
-                            <input type="number" className='form-control' onClick={()=>setPop(!pop)} id='passenger-count' placeholder='Enter count' />
+                            <input type="number" className='form-control' onClick={() => setPop(!pop)} id='passenger-count' placeholder='Enter count' />
 
+                            <label></label>
 
 
 
@@ -75,7 +108,7 @@ function FormsDoc() {
                         {pop && (
                             <div>
                                 <div className="overlay" onClick={closePopUp}></div>
-                                <PopUp onClose={closePopUp} />
+                                <PopUp />
                             </div>
                         )}
 
@@ -88,15 +121,80 @@ function FormsDoc() {
 
 function PopUp(props) {
     // const { onClose } = props;
+    // Adult
+    const [countAdult, setCountAdult] = useState(0);
+
+    const adultIncrement = (e) => {
+        e.preventDefault();
+        setCountAdult(countAdult + 1)
+        setCountInfant(Math.min(countInfant, countAdult));
+    }
+    const adultDecrement = (e) => {
+        e.preventDefault();
+        setCountAdult(countAdult > 0 ? countAdult - 1 : 0)
+        setCountInfant(Math.min(countInfant, Math.max(0, countAdult - 1)));
+    }
+    // Child
+    const [countChild, setCountChild] = useState(0);
+
+    const childIncrement = (e) => {
+        e.preventDefault();
+        setCountChild(countChild + 1)
+    }
+    const childDecrement = (e) => {
+        e.preventDefault();
+        setCountChild(countChild > 0 ? countChild - 1 : 0)
+    }
+    // Infant
+    const [countInfant, setCountInfant] = useState(0);
+
+    const infantIncrement = (e) => {
+        e.preventDefault();
+        if (countInfant < countAdult) {
+            setCountInfant(countInfant + 1)
+        }
+    }
+    const infantDecrement = (e) => {
+        e.preventDefault();
+        setCountInfant(countInfant > 0 ? countInfant - 1 : 0)
+    }
     return (
         <div className='content-wrapper'>
             <div className='popup-parent'>
                 <div className='popup'>
                     <div>
-                        <li title='My Account'><FontAwesomeIcon icon={faUser} /> My Account</li>
-                        <button title='Logout'>Logout</button>
-                        <div className='version'>
-                            <p><i>version 1.0.1</i></p>
+                        {/* Adult Count Section */}
+                        <div className='inside-count'>
+                            <label for="adult" className='form-label'>Adult:</label>
+                            <div className='inside-count__right-grp'>
+                                <button className='value-change-button' onClick={adultDecrement}>-</button>
+                                <input type="number" className='form-control' value={countAdult} id='adult' />
+                                <button className='value-change-button' onClick={adultIncrement}>+</button>
+                            </div>
+                        </div>
+                        {/* Child Count Section */}
+                        <div className='inside-count'>
+                            <label for="child" className='form-label'>Child:</label>
+                            <div className='inside-count__right-grp'>
+                                <button className='value-change-button' onClick={childDecrement}>-</button>
+                                <input type="number" className='form-control' value={countChild} id='child' />
+                                <button className='value-change-button' onClick={childIncrement}>+</button>
+                            </div>
+                        </div>
+                        {/* Infant Count Section */}
+                        <div className='inside-count'>
+                            <label for="infant" className='form-label'>Infant:</label>
+                            <div className='inside-count__right-grp'>
+                                <button className='value-change-button' onClick={infantDecrement}>-</button>
+                                <input type="number" className='form-control' value={countInfant} id='infant' />
+                                <button className='value-change-button' onClick={infantIncrement} disabled={countInfant === countAdult}>+</button>
+                            </div>
+                        </div>
+                        <div className='popup-confirm-button'>
+                            <button className='popup-confirm-button__button'>Confirm</button>
+                        </div>
+                        <div className='note'>
+                            <p><i>Note: You can only book for 1 infant per Adult</i></p>
                         </div>
                     </div>
                 </div>
